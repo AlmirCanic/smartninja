49db2f0959eeee80516ac3ffe0d1963b9063aa05
Matej Ramuta <matej.ramuta@gmail.com>
2015-06-22 16:59:22 +0200

    search candidates list implemented

---
+++ assets/js/employer/candidates-filter.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    var candidatesList = [];
+
+    $(".each-candidate").each(function() {
+        var gradeTag = $(this).attr("data-grade-tags");
+        var userId = $(this).attr("id");
+        var currentTown = $(this).attr("data-current-town");
+        var gradeScore = $(this).attr("data-grade-score");
+        var topStudent = $(this).attr("data-top-student");
+
+        gradeTag = gradeTag.replace("[", "").replace("]", "").replace(/u'/g, "").replace(/'/g, "");
+        var allTags = gradeTag.split(", ");
+
+        allTags = $.map(allTags, function(n,i){return n.toLowerCase();}); // change tags (skills) to lowercase
+
+        var currentTownList = currentTown.split(", ");
+
+        var candidate = {userId: userId, tags: allTags, score: gradeScore, topStudent: topStudent};
+
+        candidatesList.push(candidate);
+
+        //alert(candidatesList);
+    });
+
+    $("#filter-top-students").click(function() {
+        var candidate;
+        for(candidate in candidatesList) {
+            if(candidatesList[candidate].topStudent == 0) {
+                $("#"+candidatesList[candidate].userId).hide();
+            } else {
+                $("#"+candidatesList[candidate].userId).show();
+            }
+        }
+    });
+
+    $("#filter-show-all").click(function() {
+        var candidate;
+        for(candidate in candidatesList) {
+            $("#"+candidatesList[candidate].userId).show();
+        }
+    });
+
+    $("#searchButton").click(function() {
+        var skills = $("#skillSearch").val();
+
+        var skillsList = skills.split(",");
+
+        skillsList = $.map(skillsList, function(n,i){return n.toLowerCase();}); // change skills to lowercase
+
+        for(s in skillsList) {
+            if(skillsList[s].charAt(0) == " ") {
+                skillsList[s] = skillsList[s].substring(1);
+            }
+        }
+
+        var candidate;
+        for(candidate in candidatesList) {
+            var canId = $("#"+candidatesList[candidate].userId); //.hide();
+            var user = candidatesList[candidate];
+
+            for(ss in skillsList) {
+                if(jQuery.inArray(skillsList[ss], user.tags) === -1) {
+                    canId.hide();
+                }
+            }
+        }
+    });
+});
+
