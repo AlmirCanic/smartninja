c8d3216d48e6b57de46b2fabd48a123d34a44351
Matej Ramuta <matej.ramuta@gmail.com>
2015-06-23 12:10:05 +0200

    if empty search field, show all candidates; also search by location

--- assets/js/employer/candidates-filter.js
+++ assets/js/employer/candidates-filter.js
@@ -8,12 +8,15 @@ $(document).ready(function() {
         var gradeScore = $(this).attr("data-grade-score");
         var topStudent = $(this).attr("data-top-student");
 
+        // remove python list brackets and unicode u'
         gradeTag = gradeTag.replace("[", "").replace("]", "").replace(/u'/g, "").replace(/'/g, "");
         var allTags = gradeTag.split(", ");
-
         allTags = $.map(allTags, function(n,i){return n.toLowerCase();}); // change tags (skills) to lowercase
 
         var currentTownList = currentTown.split(", ");
+        currentTownList = $.map(currentTownList, function(n,i){return n.toLowerCase();}); // change towns to lowercase
+
+        allTags = allTags.concat(currentTownList);
 
         var candidate = {userId: userId, tags: allTags, score: gradeScore, topStudent: topStudent};
 
@@ -44,12 +47,22 @@ $(document).ready(function() {
 
     $("#searchButton").click(function(e) {
         e.preventDefault();
+
         var skills = $("#skillSearch").val();
 
-        var skillsList = skills.split(",");
+        // if search input field is empty, show all candidates
+        if(!skills) {
+            $(".each-candidate").each(function() {
+                $(this).show();
+            });
+            return;
+        }
+
+        var skillsList = skills.split(","); // put skills from input into array
 
         skillsList = $.map(skillsList, function(n,i){return n.toLowerCase();}); // change skills to lowercase
 
+        // remove redundant white spaces
         for(s in skillsList) {
             if(skillsList[s].charAt(0) == " ") {
                 skillsList[s] = skillsList[s].substring(1);
