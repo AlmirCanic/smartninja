7173c223efb13d47f6668704c8676d5c0c80b13a
Matej Ramuta <matej.ramuta@gmail.com>
2015-08-21 15:18:42 +0200

    laptop removed from public course application

--- app/handlers/apply.py
+++ app/handlers/apply.py
@@ -92,7 +92,7 @@ class PublicCourseApplicationAddHandler(Handler):
             address = self.request.get("address")
             dob = self.request.get("dob")
             phone = self.request.get("phone")
-            laptop = self.request.get("laptop")
+            laptop = "/"
             shirt = self.request.get("shirt")
             price = self.request.get("price")
             company = self.request.get("company_invoice")
